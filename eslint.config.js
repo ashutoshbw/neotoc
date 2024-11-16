0fad55fb8d0974c80d404c0eeeb530414aa0a0ce
Ashutosh Biswas <ashutoshbw314@gmail.com>
2024-11-16 17:30:52 +0600

    chore: made eslint config less strict

--- eslint.config.js
+++ eslint.config.js
@@ -1,9 +1,12 @@
 // @ts-check
 
+import globals from 'globals';
 import eslint from '@eslint/js';
 import tseslint from 'typescript-eslint';
 
-export default tseslint.config({
-  files: ['src/**/*.ts'],
-  extends: [eslint.configs.recommended, ...tseslint.configs.recommended],
-});
+export default tseslint.config(
+  { ignores: ['dist/*'] },
+  { languageOptions: { globals: globals.browser } },
+  eslint.configs.recommended,
+  ...tseslint.configs.recommended,
+);
