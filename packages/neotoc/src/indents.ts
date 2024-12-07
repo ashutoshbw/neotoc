df0af5913a277d50c8aeeada5bd4d8f6fe76da4e
Ashutosh Biswas <ashutoshbw314@gmail.com>
2024-12-07 08:06:58 +0600

    chore: rename --font-size-percentage to --relatve-font-size

--- packages/neotoc/src/indents.ts
+++ packages/neotoc/src/indents.ts
@@ -3,7 +3,7 @@
 // That button statys in any above parent non-foldable.
 // Note that it's NOT the distance between two indent lines!
 export function indentWidth(power: number) {
-  return `calc(calc(var(--toggle-fold-btn-width) / 2) * pow(calc(100 / var(--font-size-percentage)), ${power}) + calc(var(--indent-line-width) / 2))`;
+  return `calc(calc(var(--toggle-fold-btn-width) / 2) * pow(calc(100 / var(--relative-font-size)), ${power}) + calc(var(--indent-line-width) / 2))`;
 }
 
 // The purpose of this padding is to make the distance between indent lines equal
