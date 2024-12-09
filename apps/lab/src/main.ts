aea8a4db848c6b9b2598baadac69914752daa8b6
Ashutosh Biswas <ashutoshbw314@gmail.com>
2024-12-10 00:24:56 +0600

    style: add github style light and dark theme

--- apps/lab/src/main.ts
+++ apps/lab/src/main.ts
@@ -3,5 +3,13 @@ import "./style.css";
 import "./neotoc.css";
 import neotoc from "neotoc";
 
-neotoc({ io: "article >> h* >> #sidebar", theme: "light", ellipsis: true });
-neotoc({ io: "article >> h* >> #sidebar-1", theme: "dark", ellipsis: true });
+neotoc({
+  io: "article >> h* >> #sidebar",
+  theme: "github-light",
+  ellipsis: true,
+});
+neotoc({
+  io: "article >> h* >> #sidebar-1",
+  theme: "github-dark",
+  ellipsis: true,
+});
