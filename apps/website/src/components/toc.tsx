ed8a106bf19435ab39d2a0689c4491418c2b2eea
Ashutosh Biswas <ashutoshbw314@gmail.com>
2024-12-18 12:20:21 +0600

    chore: update toc.tsx

--- apps/website/src/components/toc.tsx
+++ apps/website/src/components/toc.tsx
@@ -4,11 +4,17 @@ import { useEffect, useRef } from "react";
 import neotoc from "neotoc";
 import "./neotoc.css";
 
-export default function Toc({ from }: { from: string }) {
+export default function Toc({
+  from,
+  className,
+}: {
+  from: string;
+  className?: string;
+}) {
   const toRef = useRef<HTMLDivElement>(null);
 
   useEffect(() => {
     return neotoc({ io: `${from} >> h*`, to: toRef.current! });
   }, [from]);
-  return <div ref={toRef}></div>;
+  return <div ref={toRef} className={className}></div>;
 }
