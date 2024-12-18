002905375fed1d21b4d5b321b0a454427298f33c
Ashutosh Biswas <ashutoshbw314@gmail.com>
2024-12-18 07:46:39 +0600

    chore: setup some remark rehype plugins

--- apps/website/next.config.ts
+++ apps/website/next.config.ts
@@ -1,5 +1,7 @@
 import type { NextConfig } from "next";
 import createMDX from "@next/mdx";
+import remarkSmartypants from "remark-smartypants";
+import rehypeSlug from "rehype-slug";
 
 const nextConfig: NextConfig = {
   pageExtensions: ["js", "jsx", "md", "mdx", "ts", "tsx"],
@@ -7,6 +9,10 @@ const nextConfig: NextConfig = {
 
 const withMDX = createMDX({
   // Add markdown plugins here, as desired
+  options: {
+    remarkPlugins: [remarkSmartypants],
+    rehypePlugins: [rehypeSlug],
+  },
 });
 
 export default withMDX(nextConfig);
