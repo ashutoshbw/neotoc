b4bdefee20f4bcc7b36d5f669a5aa1d41e7abc31
Ashutosh Biswas <ashutoshbw314@gmail.com>
2024-12-21 01:33:38 +0600

    ui: relative font slider done

--- apps/website/src/components/toc/relative-font-size-slider.tsx
+++ apps/website/src/components/toc/relative-font-size-slider.tsx
@@ -1,29 +1,28 @@
 import { useContext } from "react";
 import { TocContext } from "./toc-context";
+import { Slider } from "@/components/ui/slider";
 
 export function RelativeFontSizeSlider() {
   const tc = useContext(TocContext);
-  const handleChange: React.ChangeEventHandler<HTMLInputElement> = (e) => {
-    tc?.setRelativeFontSize(+e.target.value);
-  };
 
   return (
-    <div className="p-5 border bg-zinc-100 dark:bg-zinc-900">
-      <label htmlFor="relative-font-size">
-        <code>--relative-font-size</code>:{" "}
-      </label>
-      <input
-        className="w-[50px]"
-        type="range"
-        id="relative-font-size"
-        min="90"
-        max="100"
-        step="0.1"
-        value={tc?.relativeFontSize}
-        onChange={handleChange}
-      />
-      <span>{tc?.relativeFontSize}</span>
-      <br />
-    </div>
+    tc && (
+      <div className="flex gap-2">
+        <span className="flex-shrink-0">
+          <code>--relative-font-size</code>:
+        </span>
+        <Slider
+          className="w-[80px] flex-shrink-0"
+          value={[tc.relativeFontSize]}
+          max={100}
+          min={90}
+          step={0.1}
+          onValueChange={([v]) => {
+            tc.setRelativeFontSize(v);
+          }}
+        />
+        <span>{tc.relativeFontSize}</span>
+      </div>
+    )
   );
 }
