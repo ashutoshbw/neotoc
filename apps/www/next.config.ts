8cb6a19c377128d1f43e9e79d50e8cc7b585f719
Ashutosh Biswas <ashutoshbw314@gmail.com>
2024-12-26 17:33:40 +0600

    chore: install codehike

--- apps/www/next.config.ts
+++ apps/www/next.config.ts
@@ -2,6 +2,16 @@ import type { NextConfig } from "next";
 import createMDX from "@next/mdx";
 import remarkSmartypants from "remark-smartypants";
 import rehypeSlug from "rehype-slug";
+import { remarkCodeHike, recmaCodeHike, CodeHikeConfig } from "codehike/mdx";
+
+const chConfig: CodeHikeConfig = {
+  // optional (see https://codehike.org/docs/concepts/code):
+  components: { code: "Code" },
+  // if you can't use RSC:
+  // syntaxHighlighting: {
+  //   theme: "github-dark",
+  // },
+};
 
 const nextConfig: NextConfig = {
   pageExtensions: ["js", "jsx", "md", "mdx", "ts", "tsx"],
@@ -10,8 +20,10 @@ const nextConfig: NextConfig = {
 const withMDX = createMDX({
   // Add markdown plugins here, as desired
   options: {
-    remarkPlugins: [remarkSmartypants],
+    remarkPlugins: [remarkSmartypants, [remarkCodeHike, chConfig]],
+    recmaPlugins: [[recmaCodeHike, chConfig]],
     rehypePlugins: [rehypeSlug],
+    jsx: true,
   },
 });
 
