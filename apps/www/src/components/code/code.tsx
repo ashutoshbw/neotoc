0ece42b78b596940af0e1b0a3f2b39fab23de5ec
Ashutosh Biswas <ashutoshbw314@gmail.com>
2024-12-27 23:44:45 +0600

    ui: add style to code block

--- apps/www/src/components/code/code.tsx
+++ apps/www/src/components/code/code.tsx
@@ -2,9 +2,21 @@ import { type HighlightedCode, Pre, Inline } from "codehike/code";
 import "./github-from-css.css";
 
 export function Code({ codeblock }: { codeblock: HighlightedCode }) {
-  return <Pre code={codeblock} style={codeblock.style} />;
+  return (
+    <Pre
+      className="p-2 rounded border [--ch-16:#f4f4f5] dark:[--ch-16:#18181b]"
+      code={codeblock}
+      style={codeblock.style}
+    />
+  );
 }
 
 export function InlineCode({ codeblock }: { codeblock: HighlightedCode }) {
-  return <Inline code={codeblock} style={codeblock.style} />;
+  return (
+    <Inline
+      className="px-1 py-[2px] rounded [--ch-16:#e4e4e7] dark:[--ch-16:#18181b]"
+      code={codeblock}
+      style={codeblock.style}
+    />
+  );
 }
