0c91ec7f8b8b438a9d565d8b8ed29bc452d52129
Ashutosh Biswas <ashutoshbw314@gmail.com>
2024-12-28 20:12:25 +0600

    wip

--- apps/www/src/components/code/code.tsx
+++ apps/www/src/components/code/code.tsx
@@ -4,7 +4,7 @@ import "./github-from-css.css";
 export function Code({ codeblock }: { codeblock: HighlightedCode }) {
   return (
     <Pre
-      className="my-4 p-2 rounded border [--ch-16:#f4f4f5] dark:[--ch-16:#18181b]"
+      className="my-4 p-2 rounded [--ch-16:#f4f4f5] dark:[--ch-16:#27272a]"
       code={codeblock}
       style={codeblock.style}
     />
