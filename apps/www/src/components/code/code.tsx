0470d38bd5b929677bb283ec45d4afa4cf2473cd
Ashutosh Biswas <ashutoshbw314@gmail.com>
2024-12-31 02:13:08 +0600

    ui: fix background style of code and pre

--- apps/www/src/components/code/code.tsx
+++ apps/www/src/components/code/code.tsx
@@ -4,7 +4,7 @@ import "./github-from-css.css";
 export function Code({ codeblock }: { codeblock: HighlightedCode }) {
   return (
     <Pre
-      className="my-4 p-2 rounded [--ch-16:#f4f4f5] dark:[--ch-16:#27272a] overflow-auto"
+      className="my-4 p-2 rounded [--ch-16:#f4f4f5] dark:[--ch-16:hsla(240deg_5.9%_10%/0.8)] overflow-auto"
       code={codeblock}
       style={codeblock.style}
     />
