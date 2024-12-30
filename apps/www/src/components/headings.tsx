700cee064398486f48bbf04baba14f2d0bad3007
Ashutosh Biswas <ashutoshbw314@gmail.com>
2024-12-31 01:29:20 +0600

    ui: update heading style

--- apps/www/src/components/headings.tsx
+++ apps/www/src/components/headings.tsx
@@ -19,7 +19,7 @@ export function Heading<T extends Types = "h1">({
   return (
     <As
       className={cn(
-        "scroll-mt-20 tracking-tight",
+        "scroll-mt-20 tracking-tight [&_code]:text-[1em]",
         as !== "h1" && "relative flex border-b font-semibold",
         className
       )}
