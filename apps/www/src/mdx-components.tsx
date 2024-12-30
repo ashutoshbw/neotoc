f4ac443956c908062d5e611e8b6b14415c94cbd4
Ashutosh Biswas <ashutoshbw314@gmail.com>
2024-12-30 21:48:24 +0600

    mdx: remove relative class from 'code' to allow ellipsis to appear in toc

--- apps/www/src/mdx-components.tsx
+++ apps/www/src/mdx-components.tsx
@@ -39,7 +39,7 @@ export function useMDXComponents(components: MDXComponents): MDXComponents {
     Code,
     code: (props) => (
       <code
-        className="relative rounded bg-muted px-[0.3rem] py-[0.2rem] font-mono text-sm"
+        className="rounded bg-muted px-[0.3rem] py-[0.2rem] font-mono text-sm"
         {...props}
       />
     ),
