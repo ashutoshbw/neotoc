de48a230fed6de67584295f47ff8980dbc5ffbf3
Ashutosh Biswas <ashutoshbw314@gmail.com>
2025-01-01 04:40:48 +0600

    chore: remove unnecessary version

--- apps/www/src/app/page.tsx
+++ apps/www/src/app/page.tsx
@@ -7,7 +7,6 @@ export default function Home() {
       <h1 className="text-center text-4xl font-extrabold lg:text-5xl mt-12">
         Neotoc
       </h1>
-      <p className="text-center ml-1 font-normal">v0.1.0 Beta</p>
       <h2 className="text-center font-semibod md:text-2xl text-xl mt-8">
         Easiest way to make <i>intuitive</i> Table of Contents UI for your
         webpage.
