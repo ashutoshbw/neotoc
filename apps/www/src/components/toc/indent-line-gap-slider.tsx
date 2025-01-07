9f9a6edaa63392648b808978ec376b553b7c7c23
Ashutosh Biswas <ashutoshbw314@gmail.com>
2025-01-07 18:52:47 +0600

    feat: make slider responsive

--- apps/www/src/components/toc/indent-line-gap-slider.tsx
+++ apps/www/src/components/toc/indent-line-gap-slider.tsx
@@ -3,7 +3,7 @@ import { useContext } from "react";
 import { TocContext } from "./toc-context";
 import { Slider } from "@/components/ui/slider";
 
-export function IndentLineGapSlider() {
+export function IndentLineGapSlider({ max = 20 }: { max?: number }) {
   const tc = useContext(TocContext);
 
   return (
@@ -12,18 +12,18 @@ export function IndentLineGapSlider() {
         <span className="flex-shrink-0">
           <code>--indent-line-gap</code>:
         </span>
-        <span className="flex gap-2">
+        <span className="flex gap-2 grow">
           <Slider
-            className="w-[80px] cursor-pointer"
+            className="min-w-[40px] grow cursor-pointer"
             value={[tc.indentLineGap]}
-            max={20}
+            max={max}
             min={0}
             step={0.1}
             onValueChange={([v]) => {
               tc.setIndentLineGap(v);
             }}
           />
-          <span>{tc.indentLineGap}px</span>
+          <span className="shrink-0 w-[4em]">{tc.indentLineGap}px</span>
         </span>
       </div>
     )
