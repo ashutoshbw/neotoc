f4a058e1f02ad59bb2071d906d7b01385e5c8253
Ashutosh Biswas <ashutoshbw314@gmail.com>
2025-01-10 10:59:26 +0600

    wip

--- apps/lab/src/main.ts
+++ apps/lab/src/main.ts
@@ -1,10 +1,12 @@
 // import "./tailwind-preflight.css";
 import "./style.css";
 import "./neotoc.css";
+import "./neotoc-override.css";
 import neotoc from "neotoc";
 
 neotoc({
   io: "article >> h* >> #sidebar",
   ellipsis: true,
   title: "On this page",
+  autoFold: true,
 });
