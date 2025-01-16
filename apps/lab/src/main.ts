5b95944a22181bdd46fd9b4e8f596831d0697845
Ashutosh Biswas <ashutoshbw314@gmail.com>
2025-01-16 10:01:55 +0600

    wip

--- apps/lab/src/main.ts
+++ apps/lab/src/main.ts
@@ -4,9 +4,22 @@ import "./neotoc.css";
 import "./neotoc-override.css";
 import neotoc from "neotoc";
 
+const breadcrumbDiv = document.querySelector(".breadcrumb");
+
 neotoc({
   io: "article >> h* >> #sidebar",
   ellipsis: true,
   title: "On this page",
-  autoFold: true,
+  onBreadcrumbChange(data) {
+    if (breadcrumbDiv) {
+      breadcrumbDiv.innerHTML = "";
+      data.forEach((item) => {
+        const a = document.createElement("a");
+        a.append(item.content);
+        a.href = item.hash;
+        a.className = "breadcrumb-item";
+        breadcrumbDiv.append(a);
+      });
+    }
+  },
 });
