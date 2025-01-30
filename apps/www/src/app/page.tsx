c4f74496aed712c4b30cf5410ebcd92752797b7b
Ashutosh Biswas <ashutoshbw314@gmail.com>
2025-01-31 03:12:39 +0600

    ui: updated home page

--- apps/www/src/app/page.tsx
+++ apps/www/src/app/page.tsx
@@ -1,54 +1,41 @@
 import Link from "next/link";
-import Image from "next/image";
-import NeotocFullTextLogoDark from "../../public/logo-full-text-dark.svg";
-import NeotocFullTextLogoLight from "../../public/logo-full-text-light.svg";
+// import Image from "next/image";
+// import NeotocFullTextLogoDark from "../../public/logo-full-text-dark.svg";
+// import NeotocFullTextLogoLight from "../../public/logo-full-text-light.svg";
 import Features from "./features";
+import { cn } from "@/lib/utils";
+import FullTextLogo from "./full-logo";
 
 export default function Home() {
   return (
-    <div className="max-w-[140ch] mx-auto pb-10">
-      <div className="flex justify-center">
-        <div className="md:w-[340px] md:h-[562px] min-w-[170px] md:mt-2 mt-3 sm:block hidden">
-          <Video />
-        </div>
-        <div className="p-4 md:ml-8 w-[450px] sm:block relative left-0 sm:left-[-10px] flex flex-col justify-center">
-          <h1 className="md:mt-24 sm:mt-10 my-8 md:block flex justify-center">
-            <span className="dark:block hidden md:relative left-[-28px]">
-              <Image
-                className="block md:w-[300px] sm:w-[260px] w-[250px]"
-                src={NeotocFullTextLogoDark}
-                alt="Neotoc Logo"
-              />
-            </span>
-            <span className="block dark:hidden md:relative left-[-28px]">
-              <Image
-                className="block md:w-[300px] sm:w-[260px] w-[250px]"
-                src={NeotocFullTextLogoLight}
-                alt="Neotoc Logo"
-              />
-            </span>
-          </h1>
-          <p className="mt-2">
-            A tool for creating hyper-smooth table of contents with JavaScript.
-          </p>
-          <div className="md:block sm:flex justify-center hidden mt-8">
-            <CTAButton />
+    <div>
+      <div className="max-w-screen-lg mx-auto">
+        <div className="flex md:flex-row flex-col">
+          <div className="md:pl-[40px] px-4 flex-shrink-[3]">
+            <h1>
+              <FullTextLogo className="lg:max-w-96 max-w-80 lg:ml-0 lg:mt-24 md:mt-16 mt-8 mx-auto" />
+              <span className="sr-only">Neotoc</span>
+            </h1>
+            <h2 className="mt-8 lg:ml-10 lg:text-left text-center lg:text-2xl md:text-xl text-lg font-extralight">
+              A tool for generating{" "}
+              <b className="font-extrabold">
+                highly interactive table of contents
+              </b>{" "}
+              with <b className="font-extrabold">JavaScript</b>.
+            </h2>
+            <div className="lg:ml-10 md:flex flex-col hidden gap-2 lg:items-start items-center mt-12">
+              <CTAButton />
+              <StarOnGithubButton />
+            </div>
           </div>
-          <div className="md:block sm:flex justify-center hidden mt-3">
-            <StarOnGithubButton />
+          <div className="flex md:justify-end justify-center flex-shrink-1">
+            <Video />
           </div>
         </div>
-      </div>
-      <div className="flex gap-2 justify-center my-10 flex-wrap">
-        <div className="sm:hidden block">
+        <div className="lg:ml-12 md:hidden flex gap-3 items-start lg:justify-start justify-center mt-10 flex-wrap mx-5">
           <CTAButton />
-        </div>
-        <div className="sm:hidden block">
           <StarOnGithubButton />
         </div>
-      </div>
-      <div className="sm:hidden flex justify-center w-[270px] mx-auto">
-        <Video />
       </div>
       <Features />
     </div>
@@ -56,31 +43,73 @@ export default function Home() {
 }
 
 function Video() {
+  const borderImageBlock = `[border-image:linear-gradient(to_bottom,_transparent_0%,_hsl(var(--border))_40px,_hsl(var(--border))_calc(100%-40px),transparent_100%)_1]`;
+  const borderImageInline = `[border-image:linear-gradient(to_right,_transparent_0%,_hsl(var(--border))_40px,_hsl(var(--border))_calc(100%-40px),transparent_100%)_1]`;
   return (
-    <>
+    <div className="relative m-[40px]">
+      <div
+        className={cn(
+          "absolute left-0 w-0 h-[calc(100%+80px)] top-[-40px] border-l",
+          borderImageBlock
+        )}
+      ></div>
+      <div
+        className={cn(
+          "absolute right-0 w-0 h-[calc(100%+80px)] top-[-40px] border-l",
+          borderImageBlock
+        )}
+      ></div>
+      <div
+        className={cn(
+          "absolute top-0 h-0 w-[calc(100%+80px)] left-[-40px] border-t",
+          borderImageInline
+        )}
+      ></div>
+      <div
+        className={cn(
+          "absolute bottom-0 h-0 w-[calc(100%+80px)] left-[-40px] border-t",
+          borderImageInline
+        )}
+      ></div>
       <div className="dark:block hidden">
-        <video autoPlay loop muted playsInline>
+        <video
+          width="340"
+          height="562"
+          className="[aspect-ratio:340/562]"
+          autoPlay
+          loop
+          muted
+          playsInline
+        >
           <source src="neotoc-dark.mp4" type="video/mp4" />
           Your browser does not support the video tag.
         </video>
       </div>
       <div className="dark:hidden block">
-        <video autoPlay loop muted playsInline>
+        <video
+          width="340"
+          height="562"
+          className="[aspect-ratio:340/562]"
+          autoPlay
+          loop
+          muted
+          playsInline
+        >
           <source src="neotoc-light.mp4" type="video/mp4" />
           Your browser does not support the video tag.
         </video>
       </div>
-    </>
+    </div>
   );
 }
 
 function CTAButton() {
   return (
     <Link
-      className="inline-block transition text-xl text-white shadow-lg shadow-purple-500/10 bg-gradient-to-br from-purple-600/80 to-red-600/80 hover:bg-secondary border border-zinc-500/40 hover:border-zinc-500/80 rounded-xl py-2 px-4 hover:translate-y-[-2px]"
-      href="/docs#get-started"
+      className="inline-block transition text-xl text-center text-white shadow-lg shadow-purple-500/10 bg-gradient-to-br from-purple-600/80 to-red-600/80 hover:bg-secondary border border-zinc-500/40 hover:border-zinc-500/80 rounded-xl py-2 px-4 hover:translate-y-[-2px]"
+      href="/docs"
     >
-      ✨ See it in action
+      🚀 Get started
     </Link>
   );
 }
