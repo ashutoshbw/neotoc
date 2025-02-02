5c7b55de2895610eb4bf8aa6de7a468dfa004469
Ashutosh Biswas <ashutoshbw314@gmail.com>
2025-02-02 13:44:06 +0600

    chore: updated not found page

--- apps/www/src/app/not-found.tsx
+++ apps/www/src/app/not-found.tsx
@@ -3,10 +3,10 @@ import Link from "next/link";
 export default function NotFound() {
   return (
     <div className="text-center">
-      <h2 className="text-4xl font-extrabold mt-20">Page Not Found</h2>
+      <h2 className="text-4xl font-extrabold mt-20">Not Found</h2>
       <p className="mt-4">Could not find requested resource.</p>
       <Link className="hover:underline" href="/">
-        Return Home
+        Go Home!
       </Link>
     </div>
   );
