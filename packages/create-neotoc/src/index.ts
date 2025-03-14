552b0f80d1d22f44aea67afbb0933548fa073bfa
Ashutosh Biswas <ashutoshbw314@gmail.com>
2025-03-14 09:44:01 +0600

    wip

--- packages/create-neotoc/src/index.ts
+++ packages/create-neotoc/src/index.ts
@@ -1,1 +1,8 @@
 #!/usr/bin/env node
+
+import { parseArgs, validateArgs } from './parse-args';
+
+const r = parseArgs();
+validateArgs(r);
+
+// console.log(r);
---
+++ packages/create-neotoc/src/parse-args.ts
@@ -0,0 +1,59 @@
+import { parseArgs as pa, type ParseArgsConfig } from 'node:util';
+
+const argsConfig: ParseArgsConfig = {
+  options: {
+    base: { type: 'string', short: 'b' },
+    colors: { type: 'string', short: 'c' },
+    help: { type: 'boolean', short: 'h' },
+    version: { type: 'boolean', short: 'v' },
+  },
+  allowPositionals: true,
+};
+
+interface Args {
+  dirname?: string;
+  base?: string;
+  colors?: string;
+  help?: boolean;
+  version?: boolean;
+}
+
+export function parseArgs() {
+  try {
+    const parsedArgs = pa(argsConfig);
+    const result: Args = {
+      dirname: parsedArgs.positionals[0],
+      base: parsedArgs.values.base as string,
+      colors: parsedArgs.values.colors as string,
+      help: parsedArgs.values.help as boolean,
+      version: parsedArgs.values.version as boolean,
+    };
+    return result;
+  } catch (err) {
+    if (err instanceof Error) {
+      console.error(err.message);
+      process.exit(1);
+    }
+    throw err;
+  }
+}
+
+const baseOptions = ['plain', 'modern'];
+const colorsOptions = ['monochrome', 'zinc', 'slate'];
+
+export function validateArgs(args: Args) {
+  if (args.dirname) {
+    // check dir existence
+  }
+
+  if (args.base && !baseOptions.includes(args.base)) {
+    console.error(`Unknown value '${args.base}' for '--base, -b' option`);
+    console.error(`Available values:\n- ${baseOptions.join('\n- ')}`);
+    process.exit(1);
+  }
+  if (args.colors && !colorsOptions.includes(args.colors)) {
+    console.error(`Unknown value '${args.colors}' for '--colors, -c' option`);
+    console.error(`Available values:\n- ${colorsOptions.join('\n- ')}`);
+    process.exit(1);
+  }
+}
