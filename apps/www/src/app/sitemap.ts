b6e97a963f44558c72e05885926a4eea31c8652e
Ashutosh Biswas <ashutoshbw314@gmail.com>
2025-03-19 00:44:54 +0600

    chore: update sitemap

--- apps/www/src/app/sitemap.ts
+++ apps/www/src/app/sitemap.ts
@@ -1,4 +1,4 @@
-import { lastModified } from "./docs/docs.mdx";
+import { lastModified } from "./docs.mdx";
 
 import type { MetadataRoute } from "next";
 
@@ -9,22 +9,6 @@ export default function sitemap(): MetadataRoute.Sitemap {
     {
       url: baseURL,
       lastModified: lastModified,
-    },
-    {
-      url: `${baseURL}/docs`,
-      lastModified: lastModified,
-    },
-    {
-      url: baseURL,
-      videos: [
-        {
-          title: "Neotoc Demo",
-          thumbnail_loc: `${baseURL}/twitter-image.jpg`,
-          description:
-            "Neotoc is a tool for easily generating beautiful table of contents with JavaScript.",
-          content_loc: `${baseURL}/neotoc-dark.mp4`,
-        },
-      ],
     },
   ];
 }
