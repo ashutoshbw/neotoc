366ea618fd80e5b142946eec83f0463a0fa4e800
Ashutosh Biswas <ashutoshbw314@gmail.com>
2025-03-19 22:26:46 +0600

    chore: update doc

--- apps/www/src/app/page.tsx
+++ apps/www/src/app/page.tsx
@@ -25,7 +25,7 @@ export default function DocsPage() {
         const a = h.firstChild;
         const span = document.createElement("span");
         span.append(
-          ...[...a!.childNodes].slice(1, -1).map((n) => n.cloneNode(true))
+          ...[...a!.childNodes].slice(1, -1).map((n) => n.cloneNode(true)),
         );
         return span;
       },
@@ -52,7 +52,7 @@ export default function DocsPage() {
   }
 
   return (
-    <div className="grid md:grid-cols-[1fr_minmax(0,596px)_280px_1fr] grid-cols-1 mb-8">
+    <div className="grid md:grid-cols-[1fr_minmax(0,800px)_280px_1fr] grid-cols-1 mb-8">
       <article
         onClick={handleArticleClick}
         className="md:col-start-2 md:[font-size:0.98rem] mt-8 md:px-8 px-4"
@@ -62,7 +62,7 @@ export default function DocsPage() {
       <aside
         className={cn(
           "md:[font-size:0.92rem] md:min-h-80 md:block md:static fixed md:border-none transition-all duration-300 border-l border-b rounded-bl-lg overflow-hidden md:overflow-visible md:shadow-none shadow-lg shadow-zinc-950/10 dark:shadow-zinc-950 w-[280px] flex-shrink-0 [&>*:first-child]:sticky [&>*:first-child]:top-[calc(var(--site-header-height)+var(--top-breathing-space))]",
-          tocVisibility ? "right-0" : "right-[-300px]"
+          tocVisibility ? "right-0" : "right-[-300px]",
         )}
         id="sidebar"
       ></aside>
